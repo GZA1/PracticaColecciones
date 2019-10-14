15faa0b38cd07e7a001b6a9d59248f87b26ef906
Khalifa96 <gsenov@gmail.com>
2019-10-15 01:15:32 +0200

    Finished
    Pide los numeros separados por guiones "-"
    Han de ser Ints
    Sin espacios
    No vuelve a empezar, simplemente muestra la excepcion

---
+++ PracticaColecciones/PracticaColecciones/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaColecciones
+{
+    public class Calculadora
+    {
+        private static readonly Calculadora c = new Calculadora();
+        
+
+        private Calculadora()
+        {
+            
+        }
+
+        public static Calculadora getCalculadora() 
+        {
+            return c;
+        }
+
+        public int calcularMedia(List<int> numeros) {
+            int resultado = 0;
+            foreach (int num in numeros)
+            {
+                resultado += num;
+            }
+            return resultado;
+        }
+
+
+    }
+}
---
+++ PracticaColecciones/PracticaColecciones/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PracticaColecciones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = Calculadora.getCalculadora();
+
+            Console.Write("Intruduzca los números sobre los que desee calcular la media \n" +
+                "separados por un guión \"-\": \t");
+            String numerosRaw = Console.ReadLine();
+            String[] numeros = numerosRaw.Split("-");
+            List<int> nums = new List<int>();
+            foreach (String s in numeros)
+            {
+                try
+                {
+                    nums.Add(int.Parse(s));
+                }
+                catch (Exception ex) {
+                    Console.Write("Error\n"+ex.GetBaseException()+"\n");
+                    Console.ReadKey();
+                    System.Environment.Exit(1);
+                }
+            }
+
+            double media = 0;
+
+            foreach(int i in nums)
+            {
+                media += i;
+            }
+                media = media / nums.Count;
+
+            Console.Write(media + "\n");
+            Console.ReadKey();
+        }
+    }
+}
